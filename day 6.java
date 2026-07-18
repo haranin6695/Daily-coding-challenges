099e44e30b414531a817989899036d33b7504c9d
HARANI N <haraninatarajan6695@gmail.com>
2026-07-18 08:22:09 +0530

    Update day 6.java

--- day 6.java
+++ day 6.java
@@ -84,3 +84,27 @@ public class MinStack {
         System.out.println(minStack.getMin()); // -2
     }
 }
+
+
+problem :165
+problem name:compare version number
+category:medium
+solution:
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] rev1 = version1.split("\\.");
+        String[] rev2 = version2.split("\\.");
+        
+        int n = Math.max(rev1.length, rev2.length);
+        
+        for (int i = 0; i < n; i++) {
+            int v1 = (i < rev1.length) ? Integer.parseInt(rev1[i]) : 0;
+            int v2 = (i < rev2.length) ? Integer.parseInt(rev2[i]) : 0;
+            
+            if (v1 < v2) return -1;
+            if (v1 > v2) return 1;
+        }
+        
+        return 0; // all revisions equal
+    }
+}
