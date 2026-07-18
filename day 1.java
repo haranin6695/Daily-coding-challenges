c0bfa7b37cd44f0d475ca69ef3a0dfab3a782afe
HARANI N <haraninatarajan6695@gmail.com>
2026-07-18 08:00:45 +0530

    Update day 1.java

--- day 1.java
+++ day 1.java
@@ -105,3 +105,55 @@ class Solution {
         System.out.println("Extra test 4: " + solution.candy(ratings6) + " (Expected: 1)");
     }
 }
+
+
+problem:130
+problem name:surrounding regions
+language:java
+category:medium
+solution:
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        // Step 1: Mark all 'O's connected to the border as safe ('#')
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+        
+        // Step 2 & 3: Capture surrounded 'O's, restore safe ones
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';       // surrounded -> captured
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';       // safe -> restore
+                }
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') {
+            return;
+        }
+        
+        board[i][j] = '#'; // mark as safe
+        
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
