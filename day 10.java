2992988996428a4e5789356a7e4a81449a2898e4
HARANI N <haraninatarajan6695@gmail.com>
2026-07-20 21:40:51 +0530

    Add files via upload

---
+++ day 10.java
@@ -0,0 +1,43 @@
+problem 3300:
+problem name: Minimum Element After Replacement With Digit Sum
+category :easy
+solution
+
+class Solution {
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            min = Math.min(min, sum);
+        }
+        return min;
+    }
+}
+
+problem 704:
+problem name:binary search
+category:easy
+solution:
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
