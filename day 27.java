a7e15339fc8c9a78faf15fb3bbee15e73bb91de9
HARANI N <haraninatarajan6695@gmail.com>
2026-08-08 08:19:19 +0530

    Add solutions for linked list cycle and valid palindrome II

---
+++ day 27.java
@@ -0,0 +1,56 @@
+problem:141
+problem name:linked list cycle
+category:easy
+solution:
+  public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
+problem:680
+problem name:valid palindrome II
+category:EASY
+solution:
+  class Solution {
+    public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
