dc12dd9f83f95582b164e783a94efa8e7559b07a
HARANI N <haraninatarajan6695@gmail.com>
2026-09-19 06:43:00 +0530

    Add solutions for searching BST and finding pivot index

---
+++ day 59.java
@@ -0,0 +1,60 @@
+problem:700
+problem name:Search in a Binary Search Tree
+category:easy
+  /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode curr = root;
+
+        while(curr != null) {
+            if(curr.val == val) {
+                return curr;
+            } else if (val > curr.val) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+        return null;
+    }
+}
+
+
+problem:724
+problem name:Find Pivot Index
+category:easy
+  // Runtime: 1 ms, faster than 92.94% of Java online submissions for Find Pivot Index.
+// Time Complexity : O(n)
+class Solution {
+    public int pivotIndex(int[] nums) {
+        // Initialize total sum of the given array...
+        int totalSum = 0;
+        // Initialize 'leftsum' as sum of first i numbers, not including nums[i]...
+        int leftsum = 0;
+        // Traverse the elements and add them to store the totalSum...
+        for (int ele : nums)
+            totalSum += ele;
+        // Again traverse all the elements through the for loop and store the sum of i numbers from left to right...
+        for (int i = 0; i < nums.length; leftsum += nums[i++])
+            // sum to the left == leftsum.
+            // sum to the right === totalSum - leftsum - nums[i]..
+            // check if leftsum == totalSum - leftsum - nums[i]...
+            if (leftsum * 2 == totalSum - nums[i])
+                return i;       // Return the pivot index...
+        return -1;      // If there is no index that satisfies the conditions in the problem statement...
+    }
+}
