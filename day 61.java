87881711cccbee013fe1a8cae35567969abbbe52
HARANI N <haraninatarajan6695@gmail.com>
2026-09-23 11:25:17 +0530

    Create day 61.java

---
+++ day 61.java
@@ -0,0 +1,33 @@
+problem:941
+problem name:Valid Mountain Array
+category:easy
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+        int l = 0;
+        int r = arr.length - 1;
+        while(l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
+        while(r - 1 > 0 && arr[r] < arr[r - 1]) r--;
+        return l == r;
+    }
+}  
+problem:977
+problem name:Squares of a Sorted Array
+category:easy
+  class Solution {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
